4f02bc73deb52809134be1279072e92ae1e46d46
TimothyKristanto <timothykristanto2208@gmail.com>
2024-03-30 11:46:10 +0700

    Apps now able to predict image and video

--- apps/app.py
+++ apps/app.py
@@ -2,27 +2,45 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os, shutil
 from ultralytics import YOLO
+import mimetypes
+mimetypes.init()
 
 app = Flask(__name__)
 
-upload_folder = os.path.join('static', 'images')
+uploadFolder = os.path.join('static', 'images')
 
-app.config['UPLOAD'] = upload_folder
-
+# app.config['UPLOAD'] = uploadFolder
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = request.files['img']
+        # save the uploaded file
+        file = request.files['uploadedMedia']
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD'], filename))
-        img = os.path.join(app.config['UPLOAD'], filename)
+        uploadedMedia = os.path.join(uploadFolder, filename)
+        file.save(uploadedMedia)
+
+        # call yolov8 model
         model = YOLO("best.pt")
-        if os.path.exists(os.path.join(app.config['UPLOAD'], "predict")):
-            shutil.rmtree(os.path.join(app.config['UPLOAD'], "predict"))
-        model.predict(img, save=True, project=app.config['UPLOAD'], name="predict")
-        annotated_img = os.path.join(app.config['UPLOAD'], "predict", filename)
-        return render_template('index.html', img=annotated_img)
+
+        # check uploaded file type with mimetype to see if the file is image or video
+        fileType = mimetypes.guess_type(uploadedMedia)[0]
+
+        print(fileType)
+
+        if fileType != None:
+            fileType = fileType.split('/')[0]
+
+        # check if predict path exist
+        if os.path.exists(os.path.join(uploadFolder, "predict")):
+            shutil.rmtree(os.path.join(uploadFolder, "predict")) # if predict path exxist then remove it
+        
+        # predict and save result in predict path
+        model.predict(uploadedMedia, save=True, project=uploadFolder, name="predict")
+        annotated_media = os.path.join(uploadFolder, "predict", filename)
+
+        return render_template('index.html', result=annotated_media, type=fileType)
+        
     return render_template('index.html')
 
 if __name__ == '__main__':
